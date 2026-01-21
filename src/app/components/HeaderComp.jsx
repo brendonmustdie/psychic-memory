fe1b2cde5e8a42dcf406d5cb42e15e9eff38674c
Brendon Kruger <91871627+brendonmustdie@users.noreply.github.com>
2026-01-21 19:17:34 +0200

    Tailwind config set-up

--- src/app/components/HeaderComp.jsx
+++ src/app/components/HeaderComp.jsx
@@ -1,8 +1,8 @@
 export default function HeaderComp({heading, subheading}) {
     return (
         <div className={""}>
-            <h1 className={"font-handwritten -mb-6"}> {subheading} </h1>
-            <h1 className={"font-serif"}> {heading} </h1>
+            <h1 className={"font-handwritten -mb-6 text-3xl"}> {subheading} </h1>
+            <h1 className={"font-serif text-9xl"}> {heading} </h1>
         </div>
     )
 }
