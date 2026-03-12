b2125f2771056685f322535000ca7bd42ebcdcb0
Brendon Kruger <91871627+brendonmustdie@users.noreply.github.com>
2026-03-12 12:50:51 +0200

    Unmatched Page

--- src/app/projects/unmatcheddeck/page.js
+++ src/app/projects/unmatcheddeck/page.js
@@ -7,20 +7,57 @@ import NextProjectComp from "@/app/components/NextProjectComp";
 import WarningLabelComp from "@/app/components/WarningLabelComp";
 import ImageSlider from "@/app/components/ImageDisplayComp";
 import ImageDisplayComp from "@/app/components/ImageDisplayComp";
+import GalleryComp from "@/app/components/GalleryComp";
 
 
 export default function Page() {
-    const illustrations=[
-        {image: "/images/nature_unmatched/ElainesGetOut.png"},
-        {image: "/images/nature_unmatched/FrankBlocksThePath.png"},
-        {image: "/images/nature_unmatched/FrankWandersOff.png"},
-        {image: "/images/nature_unmatched/knight3.png"},
-        {image: "/images/nature_unmatched/pizzabox.png"},
-        {image: "/images/nature_unmatched/planted.png"},
-        {image: "/images/nature_unmatched/pruning.png"},
-        {image: "/images/nature_unmatched/resistTheUrge.png"},
-        {image: "/images/nature_unmatched/soup.png"},
-        {image: "/images/nature_unmatched/SurpriseBumSmack.png"},
+    const illustrations = [
+        {
+            image: "/images/nature_unmatched/FrankBlocksThePath.png",
+            title: "Frank Blocks The Path",
+            description: "This is a digital illustration of a dog named Frank, blocking the path.",
+        },
+        {
+            image: "/images/nature_unmatched/FrankWandersOff.png",
+            title: "Frank Wanders Off",
+            description: "This is a digital illustration of a dog named Frank, wandering off.",
+
+        },
+        {
+            image: "/images/nature_unmatched/knight3.png",
+            title: "Nazgul's Recycling",
+            description: "This is a digital illustration of a Lord of The Rings Nazgul, in a recycling logo.",
+        },
+        {
+            image: "/images/nature_unmatched/pizzabox.png",
+            title: "Mr Perfect",
+            description: "This is a digital illustration of a pizza box with from Pizza Perfect, where the drawings on the box reference inside jokes",
+        },
+        {
+            image: "/images/nature_unmatched/planted.png",
+            title: "Planted",
+            description: "This is a digital illustration of a seedling, planted by yellow gloves.",
+        },
+        {
+            image: "/images/nature_unmatched/pruning.png",
+            title: "Pruning",
+            description: "This is a digital illustration of a red pair of pruning scissors.",
+        },
+        {
+            image: "/images/nature_unmatched/resistTheUrge.png",
+            title: "Resist The Urge",
+            description: "This is a digital illustration of a couch being dragged over the floor..",
+        },
+        {
+            image: "/images/nature_unmatched/soup.png",
+            title: "No Soup For You",
+            description: "This is a digital illustration of a spilled bowl of tomato soup.",
+        },
+        {
+            image: "/images/nature_unmatched/SurpriseBumSmack.png",
+            title: "Drain Cat's Bum Smack",
+            description: "This is a digital illustration of a cat named Elaine, who we rescued from a drain, raising her paw to smack.",
+        },
 
     ]
     return (
@@ -43,20 +80,23 @@ export default function Page() {
                         content={
                             <>
                                 <p>
-                                    Unmatched is a card-based board game. It centres around the idea of unbalanced characters (Dracula, Sherlock Holmes, Invisible Man, Dr Jekyll & Mr Hyde) with their own unique deck of cards.
+                                    Unmatched is a card-based board game. It centres around the idea of unbalanced
+                                    characters (Dracula, Sherlock Holmes, Invisible Man, Dr Jekyll & Mr Hyde) with their
+                                    own unique deck of cards.
                                 </p>
                                 <p>
                                     There are hundreds of fan-made characters, with fan-made decks.
                                 </p>
                                 <p>
-                                    When someone I love became obsessed with Unmatched, I knew exactly what had to be done; He needed a custom deck based on himself.
+                                    When someone I love became obsessed with Unmatched, I knew exactly what had to be
+                                    done; He needed a custom deck based on himself.
                                 </p>
                             </>
                         }
                     />
                 }
             />
-        <ImageDisplayComp imageArray={illustrations}></ImageDisplayComp>
+            <GalleryComp images={illustrations}></GalleryComp>
         </main>
     );
 }
