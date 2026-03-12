ac74201b43439792672b8ebbc46f96eba329f194
Brendon Kruger <91871627+brendonmustdie@users.noreply.github.com>
2026-03-12 12:43:07 +0200

    Nourish Page

--- src/app/components/GalleryComp.jsx
+++ src/app/components/GalleryComp.jsx
@@ -1,9 +1,104 @@
-export default function GalleryComp({images}) {
+'use client'
+import React, {useEffect} from "react";
+import {useState} from "react";
+
+export default function GalleryComp({images, header}) {
+
+    const [selectedImage, setSelectedImage] = useState(0);
+
+    const openImage = (index) => {
+        setSelectedImage(index);
+    };
+
+    const nextImage = () => {
+        setSelectedImage((prev) => (prev + 1) % images.length);
+    };
+
+    const prevImage = () => {
+        setSelectedImage((prev) => (prev - 1 + images.length) % images.length);
+    };
+
+    useEffect(() => {
+        setSelectedImage(0);
+    }, [images]);
+
+    if (!images || images.length === 0) {
+        return <div>No images available</div>;
+    }
+
     return (
-        <div className={"m-2 grid-cols-3 grid gap-2"}>
-            {images.map((imageObject, index) => (
-                <img key={index} src={imageObject.image}></img>
-            ))}
+        <div className={"w-full"}>
+            <div className={"text-xl font-bold font-handwritten mx-auto w-1/2 text-center my-4"}>{header}</div>
+            <div className={"grid grid-cols-2 gap-1"}>
+                <div className={"grid md:col-span-2 w-full bg-white m-4 rounded-lg"}>
+                    <img className={"h-100 m-auto p-1 object-contain"} src={images[selectedImage].image} alt=""/>
+                </div>
+                <div>
+                    <div className={"grid grid-cols-2 w-full"}>
+                        <button onClick={prevImage} className={" p-2 m-2 flex flex-row group"}>
+                            <div>
+                                <div className={"group-hover:hidden flex"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                         className="bi bi-caret-left" viewBox="0 0 16 16">
+                                        <path
+                                            d="M10 12.796V3.204L4.519 8zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753"/>
+                                    </svg>
+                                    <div className={"m-2 font-handwritten"}> Previous</div>
+
+
+                                </div>
+                                <div className={"hidden group-hover:flex"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                         className="bi bi-caret-left-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"/>
+                                    </svg>
+                                    <div className={"m-2 font-handwritten font-bold"}> Previous</div>
+
+                                </div>
+                            </div>
+                        </button>
+                        <button onClick={nextImage} className={"p-2 m-2 flex flex-row group"}>
+                            <div>
+                                <div className={"group-hover:hidden flex"}>
+                                    <div className={"m-2 font-handwritten"}> Next</div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                         className="bi bi-caret-right" viewBox="0 0 16 16">
+                                        <path
+                                            d="M6 12.796V3.204L11.481 8zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753"/>
+                                    </svg>
+                                </div>
+                                <div className={"hidden group-hover:flex"}>
+                                    <div className={"m-2 font-handwritten font-bold"}> Next</div>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                         className="bi bi-caret-right-fill" viewBox="0 0 16 16">
+                                        <path
+                                            d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"/>
+                                    </svg>
+
+
+                                </div>
+                            </div>
+                        </button>
+                    </div>
+
+                    <div
+                        className={"text-white m-4 font-bold font-handwritten text-lg px-6"}>{images[selectedImage].title}</div>
+                    <div className={"text-white m-4 px-10 font-serif"}>{images[selectedImage].description}</div>
+                </div>
+            </div>
+            <div className={"m-2 grid-cols-3 grid gap-2"}>
+                {images.map((imageObject, index) => (
+                    <div key={index} className={"bg-white border-black hover:border-white border-2 m-2 rounded-lg"}>
+                        <img className={"h-80 p-1 m-auto object-cover"} key={index} src={imageObject.image}
+                             onClick={() => openImage(index)}
+                             alt={""}></img>
+                        <div>{imageObject.title}</div>
+                    </div>
+                ))}
+            </div>
         </div>
+
+
     )
 }
--- src/app/projects/nourish/page.js
+++ src/app/projects/nourish/page.js
@@ -1,3 +1,4 @@
+'use client'
 import ScreenLayoutComp from "../../components/ScreenLayoutComp";
 import HeaderComp from "../../components/HeaderComp";
 import GenericContentComp from "../../components/GenericContentComp";
@@ -7,35 +8,115 @@ import WarningLabelComp from "@/app/components/WarningLabelComp";
 import FooterComp from "@/app/components/FooterComp";
 import ImageDisplayComp from "@/app/components/ImageDisplayComp";
 import GalleryComp from "@/app/components/GalleryComp";
+import {useState} from "react";
+import DividerComp from "@/app/components/DividerComp";
+import SlidesComp from "@/app/components/SlidesComp";
 
 
 export default function Page() {
+
     const nourishImages = [
-        {image: "/images/nourish/AboutYou.png"},
-        {image: "/images/nourish/AboutYou2.png"},
-        {image: "/images/nourish/DietaryRestrictions.png"},
-        {image: "/images/nourish/Log-in.png"},
-        {image: "/images/nourish/LogYourMood.png"},
-        {image: "/images/nourish/Menu.png"},
-        {image: "/images/nourish/Onboarding.png"},
-        {image: "/images/nourish/RecommendedMenu.png"},
-        {image: "/images/nourish/Sign-up.png"},
+        {
+            image: "/images/nourish/Onboarding.png",
+            title: "Onboarding Page for Nourish App",
+            description: "This is the first page the user sees when opening the app for the first time after installing.",
+        },
+        {
+            image: "/images/nourish/Log-in.png",
+            title: "Log-in app for Nourish App",
+            description: "This is the log-in page for the nourish app",
+        },
+        {
+            image: "/images/nourish/Sign-up.png",
+            title: "Sign Up",
+            description: "This is the sign-up page for the nourish app",
+        },
+        {
+            image: "/images/nourish/AboutYou.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+        {
+            image: "/images/nourish/AboutYou2.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+        {
+            image: "/images/nourish/DietaryRestrictions.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+
+        {
+            image: "/images/nourish/LogYourMood.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+        {
+            image: "/images/nourish/Menu.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+
+        {
+            image: "/images/nourish/RecommendedMenu.png",
+            title: "About You",
+            description: "This is the page that collects the information about users in order to properly recommend meals. ",
+
+        },
+
     ]
 
     const nourishSlides = [
-        {image: "/images/nourish/nourishSlides/1.png"},
-        {image: "/images/nourish/nourishSlides/2.png"},
-        {image: "/images/nourish/nourishSlides/3.png"},
-        {image: "/images/nourish/nourishSlides/4.png"},
-        {image: "/images/nourish/nourishSlides/5.png"},
-        {image: "/images/nourish/nourishSlides/6.png"},
-        {image: "/images/nourish/nourishSlides/7.png"},
-        {image: "/images/nourish/nourishSlides/8.png"},
-        {image: "/images/nourish/nourishSlides/9.png"},
-        {image: "/images/nourish/nourishSlides/10.png"},
-        {image: "/images/nourish/nourishSlides/11.png"},
-        {image: "/images/nourish/nourishSlides/12.png"},
-        {image: "/images/nourish/nourishSlides/13.png"},
+        {
+            image: "/images/nourish/nourishSlides/1.png",
+
+        },
+        {
+            image: "/images/nourish/nourishSlides/2.png",
+
+        },
+        {
+            image: "/images/nourish/nourishSlides/3.png",
+
+        },
+        {
+            image: "/images/nourish/nourishSlides/4.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/5.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/6.png",
+
+        },
+        {
+            image: "/images/nourish/nourishSlides/7.png",
+
+        },
+        {
+            image: "/images/nourish/nourishSlides/8.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/9.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/10.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/11.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/12.png",
+        },
+        {
+            image: "/images/nourish/nourishSlides/13.png",
+        },
         {image: "/images/nourish/nourishSlides/14.png"},
         {image: "/images/nourish/nourishSlides/15.png"},
         {image: "/images/nourish/nourishSlides/16.png"},
@@ -54,14 +135,22 @@ export default function Page() {
         {image: "/images/nourish/nourishSlides/29.png"},
         {image: "/images/nourish/nourishSlides/30.png"},
     ]
+
+    const [galleryArray, setGalleryArray] = useState(nourishImages);
+    const [title, setTitle] = useState("User Interface and Experience Design");
+    const [slides, setSlides] = useState(false);
+
+    const swapView = (title, galleryArray, slides) => {
+        setGalleryArray(galleryArray);
+        setTitle(title);
+        setSlides(slides);
+    };
+
     return (
         <main className="min-h-screen">
-            <WarningLabelComp></WarningLabelComp>
             <div className={"flex flex-row"}>
                 <HomeButtonComp></HomeButtonComp>
             </div>
-
-
             <ScreenLayoutComp
                 leftComponent={
                     <HeaderComp
@@ -92,11 +181,20 @@ export default function Page() {
                     />
                 }
             />
-            <GalleryComp images={nourishImages}></GalleryComp>
-
+            <div className={"w-1/3 grid grid-cols-2 gap-1 m-auto"}>
+                <button onClick={() => swapView("User Interface and Experience Design", nourishImages, false)}
+                        className={"font-serif bg-white h-10 rounded-full text-black"}> UI/ UX Design
+                </button>
+                <button onClick={() => swapView("Brand Development", nourishSlides, true)}
+                        className={"font-serif bg-white h-10 rounded-full text-black"}> Brand Development
+                </button>
+            </div>
+            <DividerComp></DividerComp>
+            <div className={"w-full text-center text-2xl font-handwritten mt-10"}> {title} </div>
+            {slides ?
+                <SlidesComp images={galleryArray}></SlidesComp> :
+                <GalleryComp images={galleryArray}></GalleryComp>}
             <FooterComp></FooterComp>
-
-
         </main>
     );
 }
