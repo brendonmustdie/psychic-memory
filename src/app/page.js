7024ebadb00c71d00245b478a2ed57e69448da8d
Brendon Kruger <91871627+brendonmustdie@users.noreply.github.com>
2026-06-08 12:00:39 +0200

    LinkedIn

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import {useMemo} from "react";
 import Image from "next/image";
 import FlipCard from "@/app/components/FlipCard.jsx";
 import PokerChipComp from "@/app/components/PokerChipComp.jsx";
+import Link from "next/link";
 
 export default function Home() {
 
@@ -176,6 +177,12 @@ export default function Home() {
         // },
     ];
 
+    const linkedIn = <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                          className="bi bi-linkedin" viewBox="0 0 16 16">
+        <path
+            d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+    </svg>
+
     const filteredProjects =
         selectedCategory === "ALL"
             ? projects
@@ -217,7 +224,7 @@ export default function Home() {
                         <div className={"text-center text-3xl font-handwritten font-bold"}>Portfolio</div>
                     </div>
                     <div className={"text-right my-10 mx-5"}>
-
+                        <Link href={"https://www.linkedin.com/in/brendon-kruger-a5a846232/"}> {linkedIn} </Link>
                     </div>
                 </div>
                 <DividerComp></DividerComp>
